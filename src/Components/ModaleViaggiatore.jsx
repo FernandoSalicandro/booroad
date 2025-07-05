4036e2aff60b1d27252c7625599c92fd5f6eed0c
FernandoSalicandro <studio.salicandro@gmail.com>
2025-07-05 11:09:54 +0200

    feat: implementata responsiveness fino a 320px dei pulsanti modale

--- src/Components/ModaleViaggiatore.jsx
+++ src/Components/ModaleViaggiatore.jsx
@@ -1,5 +1,5 @@
 export default function ModaleViaggiatore({ utente, apriModal, chiudiModal }) {
-    const {nome, cognome, codiceFiscale, email, telefono, contattoEmergenza} = utente;
+    const { nome, cognome, codiceFiscale, email, telefono, contattoEmergenza } = utente;
     return (
         <div
             className="modal show d-block"
@@ -17,20 +17,24 @@ export default function ModaleViaggiatore({ utente, apriModal, chiudiModal }) {
                     <div className="modal-body">
                         <p>Codice Fiscale: {codiceFiscale}</p>
                         <p>Email: {email}</p>
-
-                        <p className="d-flex justify-content-between align-items-center">
-                            Numero telefono: {telefono}
+                        <div className="d-flex justify-content-between align-items-center">
+                            <p className="d-flex justify-content-between align-items-center left-col">
+                                Numero telefono: {telefono}
+                            </p>
                             <a href={`tel:${telefono}`} className="btn btn-success m-3 call-now px-3">
                                 Chiama ora
                             </a>
-                        </p>
+                        </div>
 
-                        <p className="d-flex justify-content-between align-items-center">
-                            Contatto Emergenza: {contattoEmergenza}
+                        <div className="d-flex justify-content-between align-items-center">
+                            <p className="d-flex justify-content-between align-items-center left-col">
+                                Contatto Emergenza: {contattoEmergenza}
+                            </p>
                             <a href={`tel:${contattoEmergenza}`} className="btn btn-danger m-3 call-now px-3">
                                 Chiama ora
                             </a>
-                        </p>
+                        </div>
+
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" onClick={chiudiModal}>
