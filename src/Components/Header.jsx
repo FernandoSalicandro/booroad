8172ac7831f42825b9eccda9f2c3fa38938951de
FernandoSalicandro <studio.salicandro@gmail.com>
2025-07-06 12:35:52 +0200

    feat: cambiato font weight al logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from 'react-router-dom';
 
+
+
 export default function Header() {
     return (
         <nav className="navbar position-sticky top-0 start-0 z-3 navbar-expand-lg  bg-primary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/home">BOOROAD</a>
+                <a className="navbar-brand fw-bold" href="/home">BOOROAD</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
